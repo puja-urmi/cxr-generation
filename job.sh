e3a3dfe551dda79faf6b0ef271abd64e71c14f53
Puja Saha <psaha03@uoguelph.ca>
2025-10-07 15:51:34 -0700

    Create slurm job to submit job for fine tuning on chest x ray dataset and update requirements.txt

---
+++ job.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#SBATCH --nodes=1
+#SBATCH --gres=gpu:h100:1
+#SBATCH --cpus-per-task=32
+#SBATCH --ntasks-per-node=1
+#SBATCH --mem=32000M
+#SBATCH --time=0-01:00:00
+#SBATCH --output=logs_%J.log   
+
+# Load the required modules
+module load python/3.11
+module load gcc opencv
+source /home/psaha03/scratch/env/bin/activate
+pip install -r /home/psaha03/scratch/chest-xray-classification/requirements.txt    
+
+python /home/psaha03/scratch/chest-xray-classification/train.py
