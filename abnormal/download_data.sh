54bcb552411d2559ef83a642c7f2f6cb5096f2c0
Puja Saha <psaha03@uoguelph.ca>
2025-10-08 15:10:37 -0700

    More structured organization to better manage the whole project.

---
+++ abnormal/download_data.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+curl -L -o /Users/pujasaha/Documents/iu_xray/chest-xrays-indiana-university.zip\
+  https://www.kaggle.com/api/v1/datasets/download/raddar/chest-xrays-indiana-university
